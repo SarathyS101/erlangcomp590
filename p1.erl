9af0bc1d0dd9a7a7db3557722e9f4be99cd4cdb9
SarathyS101 <se.sarathy17@gmail.com>
2026-02-12 13:09:10 -0500

    p1 & p2

---
+++ p1.erl
@@ -0,0 +1,27 @@
+-module(p1).
+-export([p1/0, factorial/1]).
+-team("Sarathy Selvam, PID: 730770538").
+p1() ->
+    {ok, Num} = io:read("Enter a number for computation: "),
+    if
+        not is_integer(Num) -> io:format("Please enter a valid integer.~n");
+        Num<0 -> 
+            Value = math:pow(abs(Num), 7),
+            io:format("The value of the expression is: ~p~n", [Value]);
+        Num == 0 -> io:format("The value of the expression is: 0~n");
+        Num>0->
+            Remainder = Num rem 7,
+            if
+                Remainder == 0 ->
+                    io:format("Fifth root of Num: ~p~n", [math:pow(Num, 1/5)]);
+                true ->
+                    Res = factorial(Num),
+                    io:format("Factorial of Num: ~p~n", [Res])
+            end
+    end.
+
+factorial(Num)->
+    if
+        Num == 0 -> 1;
+        true -> Num * factorial(Num - 1)
+    end.
---
+++ p2.erl
@@ -0,0 +1,35 @@
+-module(p2).
+-export([p2/0]).
+-team("Sarathy Selvam, PID: 730770538").
+p2() ->
+    io:format("Enter 0 to exit~n"),
+    p1().
+
+p1() ->
+    {ok, Num} = io:read("Enter a number for computation: "),
+    if
+        not is_integer(Num) -> 
+            io:format("Please enter a valid integer.~n"),
+            p1();
+        Num<0 -> 
+            Value = math:pow(abs(Num), 7),
+            io:format("The value of the expression is: ~p~n", [Value]),
+            p1();
+        Num == 0 -> io:format("0~nExit~n");
+        Num>0 ->
+            Remainder = Num rem 7,
+            if
+                Remainder == 0 ->
+                    io:format("Fifth root of Num: ~p~n", [math:pow(Num, 1/5)]);
+                true ->
+                    Res = factorial(Num),
+                    io:format("Factorial of Num: ~p~n", [Res])
+            end,
+            p1()
+    end.
+
+factorial(Num)->
+    if
+        Num == 0 -> 1;
+        true -> Num * factorial(Num - 1)
+    end.
